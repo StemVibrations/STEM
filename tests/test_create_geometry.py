a2557d16b53b094e1826778804a0e1d925b32410
noordam <aron.noordam@deltares.nl>
2023-04-25 13:30:35 +0200

    corrected test

--- tests/test_create_geometry.py
+++ tests/test_create_geometry.py
@@ -2,8 +2,8 @@ from stem.gmsh_IO import GmshIO
 
 def test_generate_mesh_2D():
     """
-    checks whether mesh data generated for 2D and 3D geometries is not empty
-    :return: -
+    Checks whether mesh data generated for 2D geometries is not empty.
+
     """
     # define the points of the surface as a list of tuples
     input_points = [(0, 0, 0), (1, 0, 0), (1, 3, 0), (0, 3, 0), (-1, 1.5, 0)]
@@ -17,15 +17,18 @@ def test_generate_mesh_2D():
     gmsh_interface = False
     # set a name for mesh output file
     mesh_output_name = "test_2D"
+    # set output directory of the mesh
+    mesh_output_dir = "."
 
     # test 2D geometry
     # define geometry dimension; input "2" for 2D
     dims = 2
     # input depth of geometry if 3D
-    depth = 0
+    extrusion_length = [0, 0, 0]
 
     gmsh_io = GmshIO()
-    mesh_data = gmsh_io.generate_gmsh_mesh(input_points, depth,mesh_size, dims, name_label, mesh_output_name,save_file,
+    mesh_data = gmsh_io.generate_gmsh_mesh(input_points, extrusion_length, mesh_size, dims, name_label,
+                                           mesh_output_name, mesh_output_dir, save_file,
                                            gmsh_interface)
 
     assert mesh_data["nodes"]["coordinates"].size > 0  # check if node_coords is not empty
@@ -36,19 +39,19 @@ def test_generate_mesh_2D():
     # check each element type contains ids and nodes
     for value in mesh_data["elements"].values():
         assert value["element_ids"].size > 0
-        assert value["element_nodes"].size > 0
+        assert value["connectivities"].size > 0
 
 
 def test_generate_mesh_3D():
     """
-    checks whether mesh data generated for 2D and 3D geometries is not empty
-    :return: -
+    Checks whether mesh data generated for 3D geometries is not empty.
+
     """
     
      # define the points of the surface as a list of tuples
     input_points = [(0, 0, 0), (1, 0, 0), (1, 3, 0), (0, 3, 0), (-1, 1.5, 0)]
     # define the mesh size
-    mesh_size = 0.1
+    element_size = 0.1
     # set a name label for the surface
     name_label = "Soil Layer"
     # if True, saves mesh data to separate mdpa files
@@ -59,14 +62,16 @@ def test_generate_mesh_3D():
     # define geometry dimension; input "3" for 3D to extrude the 2D surface
     dims = 3
     # input depth of geometry if 3D
-    depth = 1
+    extrusion_length = [0, 0, 1]
     # set a name for mesh output file
     mesh_output_name = "test_3D"
+    # set output directory of the mesh
+    mesh_output_dir = "."
 
     gmsh_io = GmshIO()
 
-    mesh_data = gmsh_io.generate_gmsh_mesh(input_points, depth, mesh_size, dims, name_label, mesh_output_name,
-                                           save_file,
+    mesh_data = gmsh_io.generate_gmsh_mesh(input_points, extrusion_length, element_size, dims, name_label,
+                                           mesh_output_name, mesh_output_dir, save_file,
                                            gmsh_interface)
 
     assert mesh_data["nodes"]["coordinates"].size > 0  # check if node_coords is not empty
@@ -77,6 +82,6 @@ def test_generate_mesh_3D():
     # check each element type contains ids and nodes
     for value in mesh_data["elements"].values():
         assert value["element_ids"].size > 0
-        assert value["element_nodes"].size > 0
+        assert value["connectivies"].size > 0
 
 
