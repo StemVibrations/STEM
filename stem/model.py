16fa23cef33a166e0403698c8db43821fa5eefc7
noordam <aronnoordam@gmail.com>
2023-05-26 09:23:04 +0200

    added function to create track geometry (WIP)

--- stem/model.py
+++ stem/model.py
@@ -1,7 +1,10 @@
 from typing import List
 
 from stem.model_part import ModelPart, BodyModelPart
+from stem.geometry import Geometry
 
+from gmsh_utils.gmsh_IO import GmshIO
+import numpy as np
 
 class Model:
     """
@@ -20,5 +23,36 @@ class Model:
         self.solver = None
         self.body_model_parts: List[BodyModelPart] = []
         self.process_model_parts: List[ModelPart] = []
+        self.geometry = Geometry()
 
+
+    def generate_track(self, sleeper_distance: float, n_sleepers: int):
+        """
+        Generates a track geometry. With rail and railpads.
+
+        Args:
+            sleeper_distance (float): distance between sleepers
+            n_sleepers (int): number of sleepers
+
+        Returns:
+
+        """
+
+        origin_point = np.array([1, 1, 1])
+        direction_vector = np.array([1, 2, 0])
+
+        geo_data = self.geometry.create_track_geometry(sleeper_distance, n_sleepers, origin_point, direction_vector)
+
+
+        return geo_data
+
+
+if __name__ == '__main__':
+    model = Model()
+
+    rail_nodes = model.generate_track(0.6, 10)
+
+
+
+    print(rail_nodes)
 
