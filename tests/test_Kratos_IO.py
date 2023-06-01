e22f6d5180a602311a2fb49edffa609d629ef19a
morettid <davide.moretti@tno.nl>
2023-06-01 14:34:39 +0200

    added point loads and their IO. Improved comments and readibility
    implementation of point load related to issue #16

--- tests/test_Kratos_IO.py
+++ tests/test_Kratos_IO.py
@@ -6,6 +6,7 @@ from stem.load import (PointLoad, Load)
 
 from tests.utils import TestUtils
 
+
 class TestKratosIO:
 
     def test_create_load_process_dictionary(self):
@@ -13,15 +14,16 @@ class TestKratosIO:
         Test the creation of the load process dictionary for the
         ProjectParameters.json file
         """
-        # define load(s)
+        # define load(s) parameters
         point_load_parameters = PointLoad(active=[True, True, True],
                                           value=[1000, 0, 0] )
 
+        # create Load objects and store in the list
         point_load = Load(name="test_name", load_parameters=point_load_parameters)
 
         all_loads = [point_load]
 
-        # write json file
+        # write dictionary for the load(s)
         kratos_io = KratosIO()
         test_dictionary = kratos_io.create_loads_process_dictionary(
             all_loads, "test_write_MaterialParameters.json"
@@ -32,6 +34,7 @@ class TestKratosIO:
             open("tests/test_data/expected_load_parameters.json")
         )
 
+        # assert the objects to be equal
         TestUtils.assert_dictionary_almost_equal(test_dictionary,
                                                  expected_load_parameters_json)
 
