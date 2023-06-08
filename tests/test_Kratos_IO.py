98ab98ca5a793a4e0f43eab3db90bc7d21ddaac7
noordam <aronnoordam@gmail.com>
2023-06-08 15:25:22 +0200

    corrected test structural material parameters

--- tests/test_Kratos_IO.py
+++ tests/test_Kratos_IO.py
@@ -55,35 +55,37 @@ class TestKratosIO:
         # compare json files using custom dictionary comparison
         TestUtils.assert_dictionary_almost_equal(written_material_parameters_json, expected_material_parameters_json)
 
-    # def test_write_structural_material_to_json(self):
-    #     """
-    #     Test writing a material list to json. In this test, the material list contains a beam material, a spring damper
-    #     material and a nodal concentrated material.
-    #
-    #     """
-    #
-    #     # Define material parameters
-    #     beam_material_parameters = EulerBeam2D(DENSITY=1.0, YOUNG_MODULUS=1.0, POISSON_RATIO=0.2)
-    #     spring_damper_material_parameters = SpringDamperLaw(NODAL_DISPLACEMENT_STIFFNESS=1.0,
-    #                                                         NODAL_DAMPING_COEFFICIENT=0.2)
-    #     nodal_concentrated_material_parameters = NodalConcentratedLaw( NODAL_MASS=1.0, NODAL_DAMPING_COEFFICIENT=0.2)
-    #
-    #     beam_material = Material("test_beam_material", material_parameters=beam_material_parameters)
-    #     spring_damper_material = Material("test_spring_damper_material",
-    #                                       material_parameters=spring_damper_material_parameters)
-    #     nodal_concentrated_material = Material("test_nodal_concentrated_material",
-    #                                            material_parameters=nodal_concentrated_material_parameters)
-    #
-    #     all_materials = [beam_material, spring_damper_material, nodal_concentrated_material]
-    #
-    #     # write json file
-    #     kratos_io = KratosIO()
-    #     kratos_io.write_material_parameters_json(all_materials, "test_write_structural_MaterialParameters.json")
-    #
-    #     # read generated json file and expected json file
-    #
-    #     written_material_parameters_json = json.load(open("test_write_structural_MaterialParameters.json"))
-    #     expected_material_parameters_json = json.load(open("tests/test_data/expected_structural_material_parameters.json"))
-    #
-    #     # compare json files using custom dictionary comparison
-    #     TestUtils.assert_dictionary_almost_equal(written_material_parameters_json, expected_material_parameters_json)
+    def test_write_structural_material_to_json(self):
+        """
+        Test writing a material list to json. In this test, the material list contains a beam material, a spring damper
+        material and a nodal concentrated material.
+
+        """
+
+        # Define material parameters
+        beam_material_parameters = EulerBeam2D(DENSITY=1.0, YOUNG_MODULUS=1.0, POISSON_RATIO=0.2, CROSS_AREA=1.0, I33=1)
+        spring_damper_material_parameters = ElasticSpringDamper(NODAL_DISPLACEMENT_STIFFNESS=[1.0,2,3],
+                                                            NODAL_DAMPING_COEFFICIENT=[0,0.2,3], NODAL_ROTATIONAL_STIFFNESS=[2.0,4,5],
+                                                                NODAL_ROTATIONAL_DAMPING_COEFFICIENT=[0,0,9])
+        nodal_concentrated_material_parameters = NodalConcentrated( NODAL_MASS=1.0, NODAL_DAMPING_COEFFICIENT=[1,2,0.2],
+                                                                    NODAL_DISPLACEMENT_STIFFNESS=[1,2,3])
+
+        beam_material = Material(id=1, name="test_beam_material", material_parameters=beam_material_parameters)
+        spring_damper_material = Material(id=2, name="test_spring_damper_material",
+                                          material_parameters=spring_damper_material_parameters)
+        nodal_concentrated_material = Material(id=3, name="test_nodal_concentrated_material",
+                                               material_parameters=nodal_concentrated_material_parameters)
+
+        all_materials = [beam_material, spring_damper_material, nodal_concentrated_material]
+
+        # write json file
+        kratos_io = KratosIO(ndim=2)
+        kratos_io.write_material_parameters_json(all_materials, "test_write_structural_MaterialParameters.json")
+
+        # read generated json file and expected json file
+
+        written_material_parameters_json = json.load(open("test_write_structural_MaterialParameters.json"))
+        expected_material_parameters_json = json.load(open("tests/test_data/expected_structural_material_parameters.json"))
+
+        # compare json files using custom dictionary comparison
+        TestUtils.assert_dictionary_almost_equal(written_material_parameters_json, expected_material_parameters_json)
