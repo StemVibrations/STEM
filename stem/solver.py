66fb203e6c1c39551157dd62d6b1d3abbc4d456f
noordam <aronnoordam@gmail.com>
2023-06-09 17:50:41 +0200

    initial commit solver settings

--- stem/solver.py
+++ stem/solver.py
@@ -1,4 +1,50 @@
+from dataclasses import dataclass
+from enum import Enum
+from abc import ABC, abstractmethod
 
+
+@dataclass
+class SchemeABC(ABC):
+    """
+    Abstract class for the scheme
+    """
+    pass
+
+
+@dataclass
+class NewmarkScheme(SchemeABC):
+    newmark_beta: float = 0.25
+    newmark_gamma: float = 0.5
+@dataclass
+class BackwardEulerScheme(SchemeABC):
+    pass
+
+class SolutionType(Enum):
+    """
+    Enum class containing the solution types
+    """
+    QUASI_STATIC = 1
+    K0_PROCEDURE = 2
+    DYNAMIC = 3
+
+@dataclass
+class SolverSettings:
+    """
+    Class containing information about the solver settings
+
+    Attributes:
+        settings (dict): dictionary containing the solver settings
+
+    """
+
+    start_time: float
+    end_time: float
+    delta_time: float
+    solution_type: SolutionType
+    reset_displacements: bool
+
+
+@dataclass
 class Solver:
     """
     Class containing information about the builder, solver and strategy
@@ -8,8 +54,11 @@ class Solver:
 
     """
 
-    def __init__(self):
+    name: str = ""
+    echo_level: int = 1
+    number_of_threads: int = 1
+    settings: dict = None
 
-        self.settings = {}
+
 
 
