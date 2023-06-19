0b0a0886b079ad9527af87e910fec4ab77325979
noordam <aronnoordam@gmail.com>
2023-06-19 16:07:59 +0200

    added some solver parameters

--- stem/solver.py
+++ stem/solver.py
@@ -2,6 +2,8 @@ from dataclasses import dataclass
 from enum import Enum
 from abc import ABC, abstractmethod
 
+from typing import Optional
+
 
 @dataclass
 class SchemeABC(ABC):
@@ -21,28 +23,54 @@ class DisplacementConvergenceCriteria(ConvergenceCriteriaABC):
     """
     Class containing information about the displacement convergence criteria
     """
-    pass
+    displacement_relative_tolerance: float = 1e-4
+    displacement_absolute_tolerance: float = 1e-9
 
 class ResidualConvergenceCriteria(ConvergenceCriteriaABC):
+    """
+    Class containing information about the residual convergence criteria
+    """
 
-    pass
+    residual_relative_tolerance: float = 1e-4
+    residual_absolute_tolerance: float = 1e-9
 
 class WaterPressureConvergenceCriteria(ConvergenceCriteriaABC):
+    """
+    Class containing information about the water pressure convergence criteria
+    """
 
-    pass
+    water_pressure_relative_tolerance: float = 1e-4
+    water_pressure_absolute_tolerance: float = 1e-9
 
 class DisplacementAndWaterPressureConvergenceCriteria(ConvergenceCriteriaABC):
+    """
+    Class containing information about the displacement and water pressure convergence criteria
+    """
 
-    pass
+    displacement_relative_tolerance: float = 1e-4
+    displacement_absolute_tolerance: float = 1e-9
+    water_pressure_relative_tolerance: float = 1e-4
+    water_pressure_absolute_tolerance: float = 1e-9
+
+
 
 @dataclass
 class NewmarkScheme(SchemeABC):
+    """
+    Class containing information about the Newmark scheme
+    """
     newmark_beta: float = 0.25
     newmark_gamma: float = 0.5
-    newmark_theta: float = 0.0
+    newmark_theta: float = 0.5
+
+
 @dataclass
 class BackwardEulerScheme(SchemeABC):
+    """
+    Class containing information about the backward Euler scheme
+    """
     pass
+
 
 class SolutionType(Enum):
     """
@@ -70,8 +98,6 @@ class LineSearchStrategy(StrategyTypeABC):
     """
     Class containing information about the line search strategy
     """
-    max_iterations: int = 15
-    min_iterations: int = 2
     max_line_search_iterations: int = 10
     first_alpha_value: float = 1.0
     second_alpha_value: float = 0.5
@@ -85,8 +111,6 @@ class ArcLengthStrategy(StrategyTypeABC):
     """
     Class containing information about the arc length strategy
     """
-    max_iterations: int = 15
-    min_iterations: int = 2
     desired_iterations: int = 10
     max_radius_factor: float = 1.0
     min_radius_factor: float = 0.1
@@ -99,12 +123,14 @@ class LinearSolverSettingsABC(ABC):
     """
     pass
 
+@dataclass
 class amgcl(LinearSolverSettingsABC):
     """
     Class containing information about the amgcl linear solver settings
     """
     scaling: bool = False
-
+    tolerance: float = 1e-6
+    max_iterations: int = 1000
 
 
 @dataclass
@@ -124,14 +150,26 @@ class SolverSettings:
     reduction_factor: float
     increase_factor: float
 
+    rebuild_level: int
     solution_type: SolutionType
     reset_displacements: bool
-    rayleigh_m: float
-    rayleigh_k: float
     strategy_type: StrategyTypeABC
     convergence_criteria: ConvergenceCriteriaABC
     scheme: SchemeABC
     linear_solver_settings: LinearSolverSettingsABC
+    rayleigh_m:Optional[float] = None
+    rayleigh_k: Optional[float] = None
+
+    def __post_init__(self):
+        """
+        Post initialization method
+
+        Raises:
+            ValueError: if the rayleigh damping parameters are not provided for dynamic analysis
+        """
+        if self.solution_type == SolutionType.DYNAMIC:
+            if self.rayleigh_m is None or self.rayleigh_k is None:
+                raise ValueError("Rayleigh damping parameters must be provided for dynamic analysis")
 
 
 @dataclass
