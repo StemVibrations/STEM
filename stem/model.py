e35a1d69ddd61d2206e26ea8449f625267693299
noordam <aronnoordam@gmail.com>
2023-07-06 10:42:33 +0200

    initial wip commit

--- stem/model.py
+++ stem/model.py
@@ -2,6 +2,7 @@ from typing import List
 
 from stem.model_part import ModelPart, BodyModelPart
 
+from gmsh_utils import gmsh_IO
 
 class Model:
     """
@@ -15,10 +16,40 @@ class Model:
 
     """
     def __init__(self):
-
+        self.ndim = None
         self.project_parameters = None
         self.solver = None
+        self.geometry = None
+        self.mesh = None
         self.body_model_parts: List[BodyModelPart] = []
         self.process_model_parts: List[ModelPart] = []
+
+
+    def add_soil_layer(self, coordinates, material_parameters, name, extrusion_length=None):
+        """
+        Adds a soil layer to the model.
+
+        Args:
+            - coordinates (np.array): The coordinates of the soil layer.
+            - material_parameters (dict): A dictionary containing the material parameters.
+
+        """
+        gmsh_io = gmsh_IO.GmshIO()
+        if self.ndim == 2:
+            gmsh_io.make_geometry_2d(coordinates, name)
+
+        if self.ndim == 3 and extrusion_length is None:
+            raise ValueError("extrusion_length must be specified for 3D models")
+
+        body_model_part = BodyModelPart()
+        body_model_part.name = name
+        body_model_part.material = material_parameters
+
+        body_model_part.set_geometry(gmsh_io.geo_data)
+
+        self.body_model_parts.append(body_model_part)
+
+        gmsh_utils.make
+
 
 
