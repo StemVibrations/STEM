c565675093cdd277c8e850c96b8d058000822cff
morettid <davide.moretti@tno.nl>
2023-07-07 17:53:39 +0200

    remove default soil materials

--- stem/default_materials.py
+++ stem/default_materials.py
@@ -15,96 +15,6 @@ class RailTypes(Enum):
     rail_60E1 = 3
 
 
-def default_peat_material() -> SoilMaterial:
-    """
-    Function to define the default elastic material for peat.
-
-    Returns:
-        - :class:`stem.structural_material.SoilMaterial`
-    """
-    # TODO fill the right materials for PEAT
-    return SoilMaterial(
-        name="default_elastic_peat",
-        soil_formulation=TwoPhaseSoil(
-            ndim=2,
-            DENSITY_SOLID=2.650,
-            POROSITY=0.3,
-            BULK_MODULUS_SOLID=1e9,
-            PERMEABILITY_XX=4.5e-30,
-            PERMEABILITY_YY=4.5e-30,
-            PERMEABILITY_XY=0.0,
-            BIOT_COEFFICIENT=1.0,
-        ),
-        constitutive_law=LinearElasticSoil(YOUNG_MODULUS=1e4, POISSON_RATIO=0.2),
-        retention_parameters=SaturatedBelowPhreaticLevelLaw(),
-        fluid_properties=FluidProperties(
-            DENSITY_FLUID=1.000,
-            DYNAMIC_VISCOSITY=8.9e-7,
-            BULK_MODULUS_FLUID=2.0e-30,
-        ),
-    )
-
-
-def default_clay_material() -> SoilMaterial:
-    """
-    Function to define the default elastic material for clay.
-
-    Returns:
-        - :class:`stem.structural_material.SoilMaterial`
-    """
-    # TODO fill the right materials for CLAY
-    return SoilMaterial(
-        name="default_elastic_clay",
-        soil_formulation=TwoPhaseSoil(
-            ndim=2,
-            DENSITY_SOLID=2.650,
-            POROSITY=0.3,
-            BULK_MODULUS_SOLID=1e9,
-            PERMEABILITY_XX=4.5e-30,
-            PERMEABILITY_YY=4.5e-30,
-            PERMEABILITY_XY=0.0,
-            BIOT_COEFFICIENT=1.0,
-        ),
-        constitutive_law=LinearElasticSoil(YOUNG_MODULUS=1e4, POISSON_RATIO=0.2),
-        retention_parameters=SaturatedBelowPhreaticLevelLaw(),
-        fluid_properties=FluidProperties(
-            DENSITY_FLUID=1.000,
-            DYNAMIC_VISCOSITY=8.9e-7,
-            BULK_MODULUS_FLUID=2.0e-30,
-        ),
-    )
-
-
-def default_sand_material() -> SoilMaterial:
-    """
-    Function to define the default elastic material for a sandy soil.
-
-    Returns:
-        - :class:`stem.structural_material.SoilMaterial`
-    """
-    # TODO fill the right materials for SAND
-    return SoilMaterial(
-        name="default_elastic_sand",
-        soil_formulation=TwoPhaseSoil(
-            ndim=2,
-            DENSITY_SOLID=2.650,
-            POROSITY=0.3,
-            BULK_MODULUS_SOLID=1e9,
-            PERMEABILITY_XX=4.5e-30,
-            PERMEABILITY_YY=4.5e-30,
-            PERMEABILITY_XY=0.0,
-            BIOT_COEFFICIENT=1.0,
-        ),
-        constitutive_law=LinearElasticSoil(YOUNG_MODULUS=1e4, POISSON_RATIO=0.2),
-        retention_parameters=SaturatedBelowPhreaticLevelLaw(),
-        fluid_properties=FluidProperties(
-            DENSITY_FLUID=1.000,
-            DYNAMIC_VISCOSITY=8.9e-7,
-            BULK_MODULUS_FLUID=2.0e-30,
-        ),
-    )
-
-
 def default_steel_rail_material(ndim: int, rail_type: RailTypes) -> StructuralMaterial:
     """
     Function to define the default elastic material for a steel beam of the given geometry.
@@ -148,10 +58,6 @@ class DefaultMaterial(Enum):
     """
     Enumeration class to retrieve default soil materials to help the user in making the model.
     """
-
-    Peat = default_peat_material()
-    Clay = default_clay_material()
-    Sand = default_sand_material()
 
     Rail_45E3_2D = default_steel_rail_material(ndim=2, rail_type=RailTypes.rail_46E3)
     Rail_45E3_3D = default_steel_rail_material(ndim=3, rail_type=RailTypes.rail_46E3)
