9debbcda4140fe2c92b1ea1afd1597604598dc96
ElenSmi <eleni.smirniou@deltares.nl>
2023-07-13 14:06:40 +0200

    Testing separate private function for mypy

--- stem/IO/kratos_water_boundaries_io.py
+++ stem/IO/kratos_water_boundaries_io.py
@@ -4,20 +4,55 @@ from stem.water_boundaries import WaterBoundary, PhreaticMultiLineBoundary, Inte
 
 
 class KratosWaterBoundariesIO:
+    """
+    Class to create the water boundary process dictionary for the ProjectParameters.json file in Kratos
+
+
+    """
 
     def __init__(self, domain: str):
+        """
+        Constructor of KratosWaterBoundariesIO class
+
+        Args:
+            domain: Name of the Kratos domain
+
+
+        """
         self.domain = domain
 
-    def __water_boundary_dict(self, name: str, type: str, water_boundary: WaterBoundaryParameters) -> Dict[str, Any]:
+    def __create_phreatic_line_dict(self, name: str, type: str, water_boundary: PhreaticLine) -> Dict[str, Any]:
+        boundary_dict_phreatic_line: Dict[str, Any] = {
+            "python_module": "apply_scalar_constraint_table_process",
+            "kratos_module": "KratosMultiphysics.GeoMechanicsApplication",
+            "process_name": "ApplyScalarConstraintTableProcess",
+            "Parameters": {
+                "model_part_name": f"{self.domain}.{name}",
+                "variable_name": "WATER_PRESSURE",
+                "is_fixed": water_boundary.is_fixed,
+                "table": [0, 0],
+                "fluid_pressure_type": type,
+                "gravity_direction": water_boundary.gravity_direction,
+                "out_of_plane_direction": water_boundary.out_of_plane_direction,
+                "specific_weight": water_boundary.specific_weight,
+                "first_reference_coordinate": water_boundary.first_reference_coordinate,
+                "second_reference_coordinate": water_boundary.second_reference_coordinate,
+                "value": water_boundary.value,
+            }
+        }
+        return boundary_dict_phreatic_line
+
+    def __create_water_boundary_dict(self, name: str, type: str, water_boundary: WaterBoundaryParameters) -> Dict[str, Any]:
         """
         Creates a dictionary containing the water boundary parameters
 
-        Attributes:
+        Args:
             - name: name of the water boundary
             - type: type of the water boundary
             - water_boundary: water boundary object
 
-        Returns: None at the moment
+        Returns:
+            - Dict[str, Any]: dictionary containing the water boundary parameters
 
         """
         if isinstance(water_boundary, PhreaticMultiLineBoundary):
@@ -59,38 +94,23 @@ class KratosWaterBoundariesIO:
             }
             return boundary_dict_interpolate
         elif isinstance(water_boundary, PhreaticLine):
-            boundary_dict_phreatic_line: Dict[str, Any] = {
-                "python_module": "apply_scalar_constraint_table_process",
-                "kratos_module": "KratosMultiphysics.GeoMechanicsApplication",
-                "process_name": "ApplyScalarConstraintTableProcess",
-                "Parameters": {
-                    "model_part_name": f"{self.domain}.{name}",
-                    "variable_name": "WATER_PRESSURE",
-                    "is_fixed": water_boundary.is_fixed,
-                    "table": [0, 0],
-                    "fluid_pressure_type": type,
-                    "gravity_direction": water_boundary.gravity_direction,
-                    "out_of_plane_direction": water_boundary.out_of_plane_direction,
-                    "specific_weight": water_boundary.specific_weight,
-                    "first_reference_coordinate": water_boundary.first_reference_coordinate,
-                    "second_reference_coordinate": water_boundary.second_reference_coordinate,
-                    "value": water_boundary.value,
-                }
-            }
+            temp_phreatic_line: PhreaticLine = water_boundary
+            boundary_dict_phreatic_line: Dict[str, Any] = self.__create_phreatic_line_dict(name, type, temp_phreatic_line)
             return boundary_dict_phreatic_line
         else:
             raise NotImplementedError("This type of boundary is not implemented")
 
-    def create_water_boundary_dict(self, water_boundary: WaterBoundary):
+    def create_water_boundary_dict(self, water_boundary: WaterBoundary) -> Dict[str, Any]:
         """
         Creates a dictionary containing the water boundary parameters
 
-        Attributes:
+        Args:
             - water_boundary: water boundary object
 
-        Returns: dictionary containing the water boundary parameters
+        Returns:
+            - Dict[str, Any]: dictionary containing the water boundary parameters
 
         """
-        return self.__water_boundary_dict(water_boundary.name,
-                                          water_boundary.water_boundary.type,
-                                          water_boundary.water_boundary)
+        return self.__create_water_boundary_dict(water_boundary.name,
+                                                 water_boundary.water_boundary.type,
+                                                 water_boundary.water_boundary)
