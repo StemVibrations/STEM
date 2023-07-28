c65a5da7e292b61bf205e1a2443d89afb21b443a
noordam <aronnoordam@gmail.com>
2023-07-28 14:20:33 +0200

    corrected utils tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,7 @@
 
-from stem.utils import *
+import numpy as np
+
+from stem.utils import Utils
 
 
 class TestUtilsStem:
@@ -14,8 +16,8 @@ class TestUtilsStem:
         p_test_1 = np.array([2, 1])
         p_test_2 = np.array([-5, 1])
 
-        assert is_collinear(point=p_test_1, start_point=p1, end_point=p2)
-        assert not is_collinear(point=p_test_2, start_point=p1, end_point=p2)
+        assert Utils.is_collinear(point=p_test_1, start_point=p1, end_point=p2)
+        assert not Utils.is_collinear(point=p_test_2, start_point=p1, end_point=p2)
 
     def test_collinearity_3d(self):
         """
@@ -28,8 +30,8 @@ class TestUtilsStem:
         p_test_1 = np.array([2, 2, -2])
         p_test_2 = np.array([2, -2, 2])
 
-        assert is_collinear(point=p_test_1, start_point=p1, end_point=p2)
-        assert not is_collinear(point=p_test_2, start_point=p1, end_point=p2)
+        assert Utils.is_collinear(point=p_test_1, start_point=p1, end_point=p2)
+        assert not Utils.is_collinear(point=p_test_2, start_point=p1, end_point=p2)
 
     def test_is_in_between_2d(self):
         """
@@ -42,8 +44,8 @@ class TestUtilsStem:
         p_test_1 = np.array([2, 2])
         p_test_2 = np.array([-1, -1])
 
-        assert not is_point_between_points(point=p_test_1, start_point=p1, end_point=p2)
-        assert is_point_between_points(point=p_test_2, start_point=p1, end_point=p2)
+        assert not Utils.is_point_between_points(point=p_test_1, start_point=p1, end_point=p2)
+        assert Utils.is_point_between_points(point=p_test_2, start_point=p1, end_point=p2)
 
     def test_is_in_between_3d(self):
         """
@@ -56,5 +58,5 @@ class TestUtilsStem:
         p_test_1 = np.array([2, 2, -2])
         p_test_2 = np.array([-1, -1, 1])
 
-        assert not is_point_between_points(point=p_test_1, start_point=p1, end_point=p2)
-        assert is_point_between_points(point=p_test_2, start_point=p1, end_point=p2)
+        assert not Utils.is_point_between_points(point=p_test_1, start_point=p1, end_point=p2)
+        assert Utils.is_point_between_points(point=p_test_2, start_point=p1, end_point=p2)
