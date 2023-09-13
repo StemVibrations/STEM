cb78b729ee745d6be1325affa1121337e626c465
noordam <aronnoordam@gmail.com>
2023-09-13 12:04:23 +0200

    added plotly plot for 2d and 3d

--- stem/plot_utils.py
+++ stem/plot_utils.py
@@ -1,11 +1,16 @@
 import numpy as np
+import numpy.typing as npt
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 # Axes3D import has side effects, it enables using projection='3d' in add_subplot
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection, PolyCollection
+import plotly.graph_objects as go
 
 # import required typing classes
-from typing import TYPE_CHECKING, List
+from typing import TYPE_CHECKING, List, Optional
+
+from stem.mesh import Mesh
+from stem.model_part import BodyModelPart, ModelPart
 
 if TYPE_CHECKING:
     from stem.geometry import Geometry, Volume, Surface
@@ -15,7 +20,7 @@ class PlotUtils:
 
     @staticmethod
     def __add_2d_surface_to_plot(geometry: 'Geometry',  surface: 'Surface', show_surface_ids,
-                                 show_line_ids, show_point_ids, ax):
+                                 show_line_ids, show_point_ids, fig):
         """
         Adds a 2D surface to a plot
 
@@ -54,6 +59,8 @@ class PlotUtils:
 
             surface_point_ids.extend(line_connectivities)
 
+        line_centroids = np.array(line_centroids)
+
         # get unique points within surface
         unique_points = []
         for point_id in surface_point_ids:
@@ -65,12 +72,11 @@ class PlotUtils:
         surface_point_coordinates = np.array([geometry.points[point_id].coordinates
                                               for point_id in unique_points])
 
-        # set vertices in format as required by Poly3DCollection
-        vertices = [list(zip(surface_point_coordinates[:, 0],
-                             surface_point_coordinates[:, 1]))]
-
-        # create Poly3DCollection
-        poly = PolyCollection(vertices, facecolors='blue', linewidths=1, edgecolors='black', alpha=0.35)
+        # add surface to plot
+        closed_loop_coordinates = np.vstack((surface_point_coordinates, surface_point_coordinates[0, :]))
+        fig.add_trace(go.Scatter(x=closed_loop_coordinates[:, 0], y=closed_loop_coordinates[:, 1], mode='lines+markers',
+                                 line={"color": 'black', "width": 1}, marker= {"color": 'red', "size": 5},
+                                 fill='toself', fillcolor="#ADD8E6"))
 
         # calculate surface centroid and add to list of all surface centroids which are required to calculate
         # the volume centroid
@@ -78,30 +84,34 @@ class PlotUtils:
 
         # show surface ids
         if show_surface_ids:
-            ax.text(surface_centroid[0], surface_centroid[1], f"s_{abs(surface.id)}",
-                    color='black', fontsize=14, fontweight='bold')
+            fig.add_trace(go.Scatter(x=[surface_centroid[0]], y=[surface_centroid[1]], mode='text',
+                                     text=f"<b>s_{abs(surface.id)}</b>", textfont={"size": 14},
+                                     textposition="middle center"))
 
         # show line ids
         if show_line_ids:
-            for line_centroid, line_k in zip(line_centroids, surface.line_ids):
-                ax.text(line_centroid[0], line_centroid[1], f"l_{abs(line_k)}",
-                        color='black', fontsize=14, fontweight='bold')
+
+            text_array = [f"<b>l_{abs(line_k)}</b>" for line_k in surface.line_ids]
+
+            fig.add_trace(go.Scatter(x=line_centroids[:, 0], y=line_centroids[:, 1], mode='text',
+                                     text=text_array, textfont={"size": 14},
+                                     textposition="top right"))
 
         # show point ids
         if show_point_ids:
-            for point_id, point in geometry.points.items():
-                ax.text(point.coordinates[0], point.coordinates[1], f"p_{point_id}",
-                        color='black', fontsize=14, fontweight='bold')
 
-        # add PolyCollection to figure
-        ax.add_collection(poly)
+            text_array = [f"<b>p_{point_id}</b>" for point_id in geometry.points.keys()]
+            point_coordinates = np.array([point.coordinates for point in geometry.points.values()])
+
+            fig.add_trace(go.Scatter(x=point_coordinates[:,0], y=point_coordinates[:,1], mode='text',
+                                     text=text_array, textfont={"size": 14}, textposition="top right"))
 
         return surface_centroid
 
 
     @staticmethod
     def __add_3d_surface_to_plot(geometry: 'Geometry', surface: 'Surface', show_surface_ids, show_line_ids,
-                                 show_point_ids, ax):
+                                 show_point_ids, fig):
         """
         Adds a 3D surface to a plot.
 
@@ -140,6 +150,8 @@ class PlotUtils:
 
             surface_point_ids.extend(line_connectivities)
 
+        line_centroids = np.array(line_centroids)
+
         # get unique points within surface
         unique_points = []
         for point_id in surface_point_ids:
@@ -151,13 +163,23 @@ class PlotUtils:
         surface_point_coordinates = np.array([geometry.points[point_id].coordinates
                                               for point_id in unique_points])
 
-        # set vertices in format as required by Poly3DCollection
-        vertices = [list(zip(surface_point_coordinates[:, 0],
-                             surface_point_coordinates[:, 1],
-                             surface_point_coordinates[:, 2]))]
+        # check which delaunay axis to use for the meshing
+        delaunayaxis = 'z'
+        if np.allclose(surface_point_coordinates[:, 0], surface_point_coordinates[0, 0]):
+            delaunayaxis = 'x'
+        elif np.allclose(surface_point_coordinates[:, 1], surface_point_coordinates[0, 1]):
+            delaunayaxis = 'y'
 
-        # create Poly3DCollection
-        poly = Poly3DCollection(vertices, facecolors='blue', linewidths=1, edgecolors='black', alpha=0.35)
+        # add surface to plot
+        fig.add_trace(go.Mesh3d(x=surface_point_coordinates[:, 0], y=surface_point_coordinates[:, 1],
+                                z=surface_point_coordinates[:, 2], opacity=0.25, showscale=False,
+                                delaunayaxis=delaunayaxis, color='blue'))
+
+        # add surface edges to plot
+        closed_loop_coordinates = np.vstack((surface_point_coordinates, surface_point_coordinates[0, :]))
+        fig.add_trace(go.Scatter3d(x=closed_loop_coordinates[:, 0], y=closed_loop_coordinates[:, 1],
+                                   z=closed_loop_coordinates[:, 2], mode='lines+markers', line={"color": 'black', "width": 2},
+                                   marker={"color": 'red', "size": 2}))
 
         # calculate surface centroid and add to list of all surface centroids which are required to calculate
         # the volume centroid
@@ -165,29 +187,37 @@ class PlotUtils:
 
         # show surface ids
         if show_surface_ids:
-            ax.text(surface_centroid[0], surface_centroid[1], surface_centroid[2], f"s_{abs(surface.id)}",
-                    color='black', fontsize=14, fontweight='bold')
+            fig.add_trace(go.Scatter3d(x=[surface_centroid[0]], y=[surface_centroid[1]],
+                                       z=[surface_centroid[2]], mode='text',
+                                       text=f"<b>s_{abs(surface.id)}</b>", textfont={"size": 14},
+                                       textposition="middle center"))
 
         # show line ids
         if show_line_ids:
-            for line_centroid, line_k in zip(line_centroids, surface.line_ids):
-                ax.text(line_centroid[0], line_centroid[1], line_centroid[2], f"l_{abs(line_k)}",
-                        color='black', fontsize=14, fontweight='bold')
+
+            text_array = [f"<b>l_{abs(line_k)}</b>" for line_k in surface.line_ids]
+
+            fig.add_trace(go.Scatter3d(x=line_centroids[:,0], y=line_centroids[:,1], z=line_centroids[:,2], mode='text',
+                                       text=text_array, textfont={"size": 14},
+                                       textposition="middle center"))
 
         # show point ids
         if show_point_ids:
-            for point_id, point in geometry.points.items():
-                ax.text(point.coordinates[0], point.coordinates[1], point.coordinates[2], f"p_{point_id}",
-                        color='black', fontsize=14, fontweight='bold')
 
-        # add Poly3DCollection to figure
-        ax.add_collection3d(poly)
+            text_array = [f"<b>p_{point_id}</b>" for point_id in geometry.points.keys()]
+            point_coordinates = np.array([point.coordinates for point in geometry.points.values()])
 
+            fig.add_trace(go.Scatter3d(x=point_coordinates[:,0], y=point_coordinates[:,1],
+                                       z=point_coordinates[:,2], mode='text',
+                                       text=text_array, textfont={"size": 14},
+                                       textposition="middle center"))
+
+        # return data, surface
         return surface_centroid
 
     @staticmethod
     def __add_3d_volume_to_plot(geometry: 'Geometry', volume: 'Volume', show_volume_ids, show_surface_ids,
-                                show_line_ids, show_point_ids, ax):
+                                show_line_ids, show_point_ids, fig):
         """
         Adds a 3D volume to a matplotlib figure.
 
@@ -210,18 +240,18 @@ class PlotUtils:
             surface = geometry.surfaces[abs(surface_k)]
 
             surface_centroid = PlotUtils.__add_3d_surface_to_plot(geometry, surface, show_surface_ids, show_line_ids,
-                                                             show_point_ids, ax)
+                                                             show_point_ids, fig)
             all_surface_centroids.append(surface_centroid)
-
         # show volume ids
         if show_volume_ids:
             volume_centroid = np.mean(all_surface_centroids, axis=0)
-            ax.text(volume_centroid[0], volume_centroid[1], volume_centroid[2], f"v_{volume.id}",
-                    color='black', fontsize=14, fontweight='bold')
+            fig.add_trace(go.Scatter3d(x=[volume_centroid[0]], y=[volume_centroid[1]], z=[volume_centroid[2]],
+                                       mode='text', text=f"<b>v_{volume.id}</b>", textfont={"size": 18},
+                                       textposition="middle center"))
 
     @staticmethod
     def create_geometry_figure(ndim: int, geometry: 'Geometry', show_volume_ids: bool = False, show_surface_ids: bool = False,
-                               show_line_ids: bool = False, show_point_ids: bool = False) -> plt.Figure:
+                               show_line_ids: bool = False, show_point_ids: bool = False) -> go.Figure:
         """
         Creates the geometry of the model in a matplotlib plot.
 
@@ -237,58 +267,74 @@ class PlotUtils:
             - plt.Figure: Figure object
 
         """
+
+
+
         # Initialize figure in 3D
-        fig = plt.figure()
+        # fig = plt.figure()
+        fig = go.Figure()
 
         if ndim == 2:
-            ax = fig.add_subplot(111)
             for surface in geometry.surfaces.values():
+
                 PlotUtils.__add_2d_surface_to_plot(geometry, surface, show_surface_ids, show_line_ids,
-                                                   show_point_ids, ax)
+                                                   show_point_ids, fig)
 
         elif ndim == 3:
-            ax = fig.add_subplot(111, projection='3d')
             # loop over all volumes
             for volume_data in geometry.volumes.values():
+
                 PlotUtils.__add_3d_volume_to_plot(geometry, volume_data, show_volume_ids, show_surface_ids,
-                                                  show_line_ids, show_point_ids, ax)
+                                                  show_line_ids, show_point_ids, fig)
+
         else:
             raise ValueError("Number of dimensions should be 2 or 3")
 
-        # set limits of plot
-        # extend limits with buffer, which is 10% of the difference between min and max
-        buffer = 0.1
-        all_coordinates = np.array([point.coordinates for point in geometry.points.values()])
+        fig.update_layout(scene=dict(xaxis={"title": "x-coordinates [m]"},
+                                     yaxis={"title": "y-coordinates [m]"},
+                                     zaxis={"title": "z-coordinates [m]"}),
+                          showlegend=False)
 
-        # calculate and set x and y limits
-        min_x, max_x = np.min(all_coordinates[:, 0]), np.max(all_coordinates[:, 0])
-        dx = max_x - min_x
-        min_y, max_y = np.min(all_coordinates[:, 1]), np.max(all_coordinates[:, 1])
-        dy = max_y - min_y
-
-        xlim = [min_x - buffer * dx, max_x + buffer * dx]
-        ylim = [min_y - buffer * dy, max_y + buffer * dy]
-
-        ax.set_xlim(xlim)
-        ax.set_ylim(ylim)
-
-        # set x and y labels
-        ax.set_xlabel("x coordinates [m]")
-        ax.set_ylabel("y coordinates [m]")
-
-        if ndim == 3:
-            # calculate and set z limits
-            min_z, max_z = np.min(all_coordinates[:, 2]), np.max(all_coordinates[:, 2])
-            dz = max_z - min_z
-
-            zlim = [min_z - buffer * dz, max_z + buffer * dz]
-
-            ax.set_zlim(zlim)
-
-            # set z label
-            ax.set_zlabel("z coordinates [m]")
-
-        # set equal aspect ratio to equal axes
-        ax.set_aspect('equal')
+        fig.write_html("test2.html")
 
         return fig
+
+if __name__ == '__main__':
+
+    pass
+    # import plotly.graph_objects as go
+    #
+    # # Create data for your 3D mesh plot
+    # # Example data
+    # x = [0, 1, 2, 3]
+    # y = [0, 1, 2, 3]
+    # z = [
+    #     [1, 2, 1, 2],
+    #     [2, 3, 2, 3],
+    #     [3, 4, 3, 4],
+    #     [4, 5, 4, 5]
+    # ]
+    #
+    # # Create the 3D mesh plot
+    # fig = go.Figure()
+    #
+    # # Add the 3D mesh plot
+    # fig.add_trace(go.Mesh3d(x=x, y=y, z=z, opacity=0.7))
+    #
+    # # # Add planes on the x-y, x-z, and y-z planes
+    # # fig.add_trace(go.Surface(x=x, y=y, z=[[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]], colorscale='viridis',
+    # #                          showscale=False))
+    # # fig.add_trace(go.Surface(x=x, y=[0, 0, 0, 0], z=z, colorscale='viridis', showscale=False))
+    # # fig.add_trace(go.Surface(x=[0, 0, 0, 0], y=y, z=z, colorscale='viridis', showscale=False))
+    #
+    # # Customize the layout if needed
+    # fig.update_layout(
+    #     scene=dict(
+    #         xaxis=dict(nticks=4),
+    #         yaxis=dict(nticks=4),
+    #         zaxis=dict(nticks=4),
+    #     )
+    # )
+    #
+    # # Show the figure
+    # fig.show()
