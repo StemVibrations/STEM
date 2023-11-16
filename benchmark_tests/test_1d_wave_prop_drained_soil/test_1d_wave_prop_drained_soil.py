a6980b4af5816a770a80f419c8197b3b94e39a37
Bruno Zuada Coelho <brunozuada@gmail.com>
2023-11-16 14:55:09 +0100

    created function to tests

--- benchmark_tests/test_1d_wave_prop_drained_soil/test_1d_wave_prop_drained_soil.py
+++ benchmark_tests/test_1d_wave_prop_drained_soil/test_1d_wave_prop_drained_soil.py
@@ -8,117 +8,119 @@ from stem.solver import AnalysisType, SolutionType, TimeIntegration, Displacemen
 from stem.output import NodalOutput, VtkOutputParameters, Output
 from stem.stem import Stem
 
-# Define geometry, conditions and material parameters
-# --------------------------------
 
-# Specify dimension and initiate the model
-ndim = 2
-model = Model(ndim)
+def test_stem():
+    # Define geometry, conditions and material parameters
+    # --------------------------------
 
-# Specify material model
-# Linear elastic drained soil with a Density of 2700, a Young's modulus of 50e6,
-# a Poisson ratio of 0.3 & a Porosity of 0.3 is specified.
-DENSITY_SOLID = 2700
-POROSITY = 0.3
-YOUNG_MODULUS = 50e6
-POISSON_RATIO = 0.3
-soil_formulation1 = OnePhaseSoil(ndim, IS_DRAINED=True, DENSITY_SOLID=DENSITY_SOLID, POROSITY=POROSITY)
-constitutive_law1 = LinearElasticSoil(YOUNG_MODULUS=YOUNG_MODULUS, POISSON_RATIO=POISSON_RATIO)
-retention_parameters1 = SaturatedBelowPhreaticLevelLaw()
-material1 = SoilMaterial("soil", soil_formulation1, constitutive_law1, retention_parameters1)
+    # Specify dimension and initiate the model
+    ndim = 2
+    model = Model(ndim)
 
-# Specify the coordinates for the column: x:1m x y:10m
-layer1_coordinates = [(0, 0, 0), (1, 0, 0), (1, 10, 0), (0, 10, 0)]
+    # Specify material model
+    # Linear elastic drained soil with a Density of 2700, a Young's modulus of 50e6,
+    # a Poisson ratio of 0.3 & a Porosity of 0.3 is specified.
+    DENSITY_SOLID = 2700
+    POROSITY = 0.3
+    YOUNG_MODULUS = 50e6
+    POISSON_RATIO = 0.3
+    soil_formulation1 = OnePhaseSoil(ndim, IS_DRAINED=True, DENSITY_SOLID=DENSITY_SOLID, POROSITY=POROSITY)
+    constitutive_law1 = LinearElasticSoil(YOUNG_MODULUS=YOUNG_MODULUS, POISSON_RATIO=POISSON_RATIO)
+    retention_parameters1 = SaturatedBelowPhreaticLevelLaw()
+    material1 = SoilMaterial("soil", soil_formulation1, constitutive_law1, retention_parameters1)
 
-# Create the soil layer
-model.add_soil_layer_by_coordinates(layer1_coordinates, material1, "soil_column")
+    # Specify the coordinates for the column: x:1m x y:10m
+    layer1_coordinates = [(0, 0, 0), (1, 0, 0), (1, 10, 0), (0, 10, 0)]
 
-# Boundary conditions and Loads
-load_coordinates = [(0.0, 10.0, 0), (1.0, 10.0, 0)]
-# Add table for the load in the mdpa file
-t = (0.0, 0.0025, 1)
-values = (0.0, -1000.0, -1000.0)
-LINE_LOAD_Y = Table(times=t, values=values)
-# Add line load
-line_load = LineLoad(active=[False, True, False], value=[0, LINE_LOAD_Y, 0])
-model.add_load_by_coordinates(load_coordinates, line_load, "load")
+    # Create the soil layer
+    model.add_soil_layer_by_coordinates(layer1_coordinates, material1, "soil_column")
 
-# Define boundary conditions
-no_displacement_parameters = DisplacementConstraint(active=[True, True, True], is_fixed=[True, True, True],
-                                                    value=[0, 0, 0])
+    # Boundary conditions and Loads
+    load_coordinates = [(0.0, 10.0, 0), (1.0, 10.0, 0)]
+    # Add table for the load in the mdpa file
+    t = (0.0, 0.0025, 1)
+    values = (0.0, -1000.0, -1000.0)
+    LINE_LOAD_Y = Table(times=t, values=values)
+    # Add line load
+    line_load = LineLoad(active=[False, True, False], value=[0, LINE_LOAD_Y, 0])
+    model.add_load_by_coordinates(load_coordinates, line_load, "load")
 
-sym_parameters = DisplacementConstraint(active=[True, False, True], is_fixed=[True, False, False], value=[0, 0, 0])
+    # Define boundary conditions
+    no_displacement_parameters = DisplacementConstraint(active=[True, True, True], is_fixed=[True, True, True],
+                                                        value=[0, 0, 0])
 
-# Add boundary conditions to the model (geometry ids are shown in the show_geometry)
-model.add_boundary_condition_by_geometry_ids(1, [1], no_displacement_parameters, "base_fixed")
-model.add_boundary_condition_by_geometry_ids(1, [2, 4], sym_parameters, "side_rollers")
+    sym_parameters = DisplacementConstraint(active=[True, False, True], is_fixed=[True, False, False], value=[0, 0, 0])
 
-# Synchronize geometry
-model.synchronise_geometry()
+    # Add boundary conditions to the model (geometry ids are shown in the show_geometry)
+    model.add_boundary_condition_by_geometry_ids(1, [1], no_displacement_parameters, "base_fixed")
+    model.add_boundary_condition_by_geometry_ids(1, [2, 4], sym_parameters, "side_rollers")
 
-# Show geometry and geometry ids
-# model.show_geometry(show_line_ids=True, show_point_ids=True)
+    # Synchronize geometry
+    model.synchronise_geometry()
 
-# Set mesh size and generate mesh
-# --------------------------------
-model.set_mesh_size(element_size=0.45)
-model.generate_mesh()
+    # Show geometry and geometry ids
+    # model.show_geometry(show_line_ids=True, show_point_ids=True)
 
-# Define project parameters
-# --------------------------------
+    # Set mesh size and generate mesh
+    # --------------------------------
+    model.set_mesh_size(element_size=0.45)
+    model.generate_mesh()
 
-# Set up solver settings
-analysis_type = AnalysisType.MECHANICAL_GROUNDWATER_FLOW
-solution_type = SolutionType.DYNAMIC
-# Set up start and end time of calculation, time step and etc
-time_integration = TimeIntegration(start_time=0.0, end_time=0.15, delta_time=0.0025, reduction_factor=1.0,
-                                   increase_factor=1.0, max_delta_time_factor=1000)
-convergence_criterion = DisplacementConvergenceCriteria(displacement_relative_tolerance=1.0E-12,
-                                                        displacement_absolute_tolerance=1.0E-6)
-strategy_type = NewtonRaphsonStrategy(min_iterations=6, max_iterations=15, number_cycles=100)
-scheme_type = NewmarkScheme(newmark_beta=0.25, newmark_gamma=0.5, newmark_theta=0.5)
-linear_solver_settings = Amgcl(tolerance=1e-8, max_iteration=500, scaling=True)
-stress_initialisation_type = StressInitialisationType.NONE
-solver_settings = SolverSettings(analysis_type=analysis_type, solution_type=solution_type,
-                                 stress_initialisation_type=stress_initialisation_type,
-                                 time_integration=time_integration,
-                                 is_stiffness_matrix_constant=False, are_mass_and_damping_constant=False,
-                                 convergence_criteria=convergence_criterion,
-                                 strategy_type=strategy_type, scheme=scheme_type,
-                                 linear_solver_settings=linear_solver_settings, rayleigh_k=6e-6,
-                                 rayleigh_m=0.02)
+    # Define project parameters
+    # --------------------------------
 
-# Set up problem data
-problem = Problem(problem_name="test_1d_wave_prop_drained_soil", number_of_threads=2, settings=solver_settings)
-model.project_parameters = problem
+    # Set up solver settings
+    analysis_type = AnalysisType.MECHANICAL_GROUNDWATER_FLOW
+    solution_type = SolutionType.DYNAMIC
+    # Set up start and end time of calculation, time step and etc
+    time_integration = TimeIntegration(start_time=0.0, end_time=0.15, delta_time=0.0025, reduction_factor=1.0,
+                                    increase_factor=1.0, max_delta_time_factor=1000)
+    convergence_criterion = DisplacementConvergenceCriteria(displacement_relative_tolerance=1.0E-12,
+                                                            displacement_absolute_tolerance=1.0E-6)
+    strategy_type = NewtonRaphsonStrategy(min_iterations=6, max_iterations=15, number_cycles=100)
+    scheme_type = NewmarkScheme(newmark_beta=0.25, newmark_gamma=0.5, newmark_theta=0.5)
+    linear_solver_settings = Amgcl(tolerance=1e-8, max_iteration=500, scaling=True)
+    stress_initialisation_type = StressInitialisationType.NONE
+    solver_settings = SolverSettings(analysis_type=analysis_type, solution_type=solution_type,
+                                    stress_initialisation_type=stress_initialisation_type,
+                                    time_integration=time_integration,
+                                    is_stiffness_matrix_constant=False, are_mass_and_damping_constant=False,
+                                    convergence_criteria=convergence_criterion,
+                                    strategy_type=strategy_type, scheme=scheme_type,
+                                    linear_solver_settings=linear_solver_settings, rayleigh_k=6e-6,
+                                    rayleigh_m=0.02)
 
-# Define the results to be written to the output file
-# Nodal results
-nodal_results = [NodalOutput.VELOCITY]
+    # Set up problem data
+    problem = Problem(problem_name="test_1d_wave_prop_drained_soil", number_of_threads=2, settings=solver_settings)
+    model.project_parameters = problem
 
-# Define the output process
-vtk_output_process = Output(
-    output_name="vtk_output",
-    output_dir="output",
-    output_parameters=VtkOutputParameters(
-        file_format="binary",
-        output_interval=1,
-        nodal_results=nodal_results,
-        gauss_point_results=[],
-        output_control_type="step"
+    # Define the results to be written to the output file
+    # Nodal results
+    nodal_results = [NodalOutput.VELOCITY]
+
+    # Define the output process
+    vtk_output_process = Output(
+        output_name="vtk_output",
+        output_dir="output",
+        output_parameters=VtkOutputParameters(
+            file_format="ascii",
+            output_interval=10,
+            nodal_results=nodal_results,
+            gauss_point_results=[],
+            output_control_type="step"
+        )
     )
-)
-# add the output process to the model
-model.output_settings = [vtk_output_process]
+    # add the output process to the model
+    model.output_settings = [vtk_output_process]
 
-# Define the kratos input folder
-input_folder = "benchmark_tests/test_1d_wave_prop_drained_soil/inputs_kratos"
+    # Define the kratos input folder
+    input_folder = "benchmark_tests/test_1d_wave_prop_drained_soil/inputs_kratos"
 
-# Write KRATOS input files
-# --------------------------------
-stem = Stem(model, input_folder)
-stem.write_all_input_files()
+    # Write KRATOS input files
+    # --------------------------------
+    stem = Stem(model, input_folder)
+    stem.write_all_input_files()
 
-# Run Kratos calculation
-# --------------------------------
-stem.run_calculation()
+    # Run Kratos calculation
+    # --------------------------------
+    stem.run_calculation()
--- benchmark_tests/test_moving_load_on_embankment_3d/test_moving_load_on_embankment_3d.py
+++ benchmark_tests/test_moving_load_on_embankment_3d/test_moving_load_on_embankment_3d.py
@@ -1,15 +1,3 @@
-import sys
-import os
-
-path_kratos = r"D:\Kratos_without_compiling"
-material_name = "MaterialParameters.json"
-project_name = "ProjectParameters.json"
-mesh_name = "calculate_moving_load_on_embankment_3d.mdpa"
-
-sys.path.append(os.path.join(path_kratos, "KratosGeoMechanics"))
-sys.path.append(os.path.join(path_kratos, r"KratosGeoMechanics\libs"))
-
-import KratosMultiphysics.GeoMechanicsApplication
 from KratosMultiphysics.GeoMechanicsApplication.geomechanics_analysis import (GeoMechanicsAnalysis)
 from stem.model import Model
 from stem.soil_material import OnePhaseSoil, LinearElasticSoil, SoilMaterial, SaturatedBelowPhreaticLevelLaw
@@ -18,153 +6,147 @@ from stem.boundary import DisplacementConstraint
 from stem.solver import AnalysisType, SolutionType, TimeIntegration, DisplacementConvergenceCriteria,\
     NewtonRaphsonStrategy, NewmarkScheme, Amgcl, StressInitialisationType, SolverSettings, Problem
 from stem.output import NodalOutput, VtkOutputParameters, Output
-from stem.IO.kratos_io import KratosIO
+from stem.stem import Stem
 
+def test_stem():
+    # Define geometry, conditions and material parameters
+    # --------------------------------
 
-# Define geometry, conditions and material parameters
-# --------------------------------
+    # Specify dimension and initiate the model
+    ndim = 3
+    model = Model(ndim)
 
-# Specify dimension and initiate the model
-ndim = 3
-model = Model(ndim)
+    # Specify material model
+    solid_density = 2650
+    porosity = 0.3
+    young_modulus = 30e6
+    poisson_ratio = 0.2
+    soil_formulation1 = OnePhaseSoil(ndim, IS_DRAINED=True, DENSITY_SOLID=solid_density, POROSITY=porosity)
+    constitutive_law1 = LinearElasticSoil(YOUNG_MODULUS=young_modulus, POISSON_RATIO=poisson_ratio)
+    retention_parameters1 = SaturatedBelowPhreaticLevelLaw()
+    material_soil1 = SoilMaterial("soil1", soil_formulation1, constitutive_law1, retention_parameters1)
+    material_soil2 = SoilMaterial("soil2", soil_formulation1, constitutive_law1, retention_parameters1)
+    material_embankment = SoilMaterial("embankment", soil_formulation1, constitutive_law1, retention_parameters1)
 
-# Specify material model
-solid_density = 2650
-porosity = 0.3
-young_modulus = 30e6
-poisson_ratio = 0.2
-soil_formulation1 = OnePhaseSoil(ndim, IS_DRAINED=True, DENSITY_SOLID=solid_density, POROSITY=porosity)
-constitutive_law1 = LinearElasticSoil(YOUNG_MODULUS=young_modulus, POISSON_RATIO=poisson_ratio)
-retention_parameters1 = SaturatedBelowPhreaticLevelLaw()
-material_soil1 = SoilMaterial("soil1", soil_formulation1, constitutive_law1, retention_parameters1)
-material_soil2 = SoilMaterial("soil2", soil_formulation1, constitutive_law1, retention_parameters1)
-material_embankment = SoilMaterial("embankment", soil_formulation1, constitutive_law1, retention_parameters1)
+    # Specify the coordinates for the column: x:2m x y:2m x z:10m
+    soil1_coordinates = [(0.0, 0.0, 0.0), (5.0, 0.0, 0.0), (5.0, 1.0, 0.0), (0.0, 1.0, 0.0)]
+    soil2_coordinates = [(0.0, 1.0, 0.0), (5.0, 1.0, 0.0), (5.0, 2.0, 0.0), (0.0, 2.0, 0.0)]
+    embankment_coordinates = [(0.0, 2.0, 0.0), (3.0, 2.0, 0.0), (1.5, 3.0, 0.0), (0.75, 3.0, 0.0), (0, 3.0, 0.0)]
+    model.extrusion_length = [0, 0, 10]
 
-# Specify the coordinates for the column: x:2m x y:2m x z:10m
-soil1_coordinates = [(0.0, 0.0, 0.0), (5.0, 0.0, 0.0), (5.0, 1.0, 0.0), (0.0, 1.0, 0.0)]
-soil2_coordinates = [(0.0, 1.0, 0.0), (5.0, 1.0, 0.0), (5.0, 2.0, 0.0), (0.0, 2.0, 0.0)]
-embankment_coordinates = [(0.0, 2.0, 0.0), (3.0, 2.0, 0.0), (1.5, 3.0, 0.0), (0.75, 3.0, 0.0), (0, 3.0, 0.0)]
-model.extrusion_length = [0, 0, 10]
+    # Create the soil layer
+    model.add_soil_layer_by_coordinates(soil1_coordinates, material_soil1, "soil1")
+    model.add_soil_layer_by_coordinates(soil2_coordinates, material_soil2, "soil2")
+    model.add_soil_layer_by_coordinates(embankment_coordinates, material_embankment, "embankment")
 
-# Create the soil layer
-model.add_soil_layer_by_coordinates(soil1_coordinates, material_soil1, "soil1")
-model.add_soil_layer_by_coordinates(soil2_coordinates, material_soil2, "soil2")
-model.add_soil_layer_by_coordinates(embankment_coordinates, material_embankment, "embankment")
+    # Define moving load
+    load_coordinates = [(0.75, 3.0, 0.0), (0.75, 3.0, 10.0)]
+    moving_load = MovingLoad(load=[0.0, -10.0, 0.0], direction=[-1, -1, 1], velocity=5, origin=[0.75, 3.0, 0.0], offset=0.0)
+    model.add_load_by_coordinates(load_coordinates, moving_load, "moving_load")
 
-# Define moving load
-load_coordinates = [(0.75, 3.0, 0.0), (0.75, 3.0, 10.0)]
-moving_load = MovingLoad(load=[0.0, -10.0, 0.0], direction=[-1, -1, 1], velocity=5, origin=[0.75, 3.0, 0.0], offset=0.0)
-model.add_load_by_coordinates(load_coordinates, moving_load, "moving_load")
+    # Define boundary conditions
+    no_displacement_parameters = DisplacementConstraint(active=[True, True, True],
+                                                        is_fixed=[True, True, True], value=[0, 0, 0])
+    roller_displacement_parameters = DisplacementConstraint(active=[True, True, True], is_fixed=[True, False, True], value=[0, 0, 0])
 
-# Define boundary conditions
-no_displacement_parameters = DisplacementConstraint(active=[True, True, True],
-                                                    is_fixed=[True, True, True], value=[0, 0, 0])
-roller_displacement_parameters = DisplacementConstraint(active=[True, True, True], is_fixed=[True, False, True], value=[0, 0, 0])
+    # Add boundary conditions to the model (geometry ids are shown in the show_geometry)
+    model.add_boundary_condition_by_geometry_ids(2, [1], no_displacement_parameters, "base_fixed")
+    model.add_boundary_condition_by_geometry_ids(2, [2, 4, 5, 6, 7, 10, 11, 12, 15, 16, 17], roller_displacement_parameters, "sides_roller")
 
-# Add boundary conditions to the model (geometry ids are shown in the show_geometry)
-model.add_boundary_condition_by_geometry_ids(2, [1], no_displacement_parameters, "base_fixed")
-model.add_boundary_condition_by_geometry_ids(2, [2, 4, 5, 6, 7, 10, 11, 12, 15, 16, 17], roller_displacement_parameters, "sides_roller")
+    # Synchronize geometry
+    model.synchronise_geometry()
 
-# Synchronize geometry
-model.synchronise_geometry()
+    # Show geometry and geometry ids
+    model.show_geometry(show_surface_ids=True)
 
-# Show geometry and geometry ids
-model.show_geometry(show_surface_ids=True)
+    # Set mesh size and generate mesh
+    # --------------------------------
+    model.set_mesh_size(element_size=5)
+    model.generate_mesh()
 
-# Set mesh size and generate mesh
-# --------------------------------
-model.set_mesh_size(element_size=5)
-model.generate_mesh()
+    # Define project parameters
+    # --------------------------------
 
-# Define project parameters
-# --------------------------------
+    # Set up solver settings
+    analysis_type = AnalysisType.MECHANICAL_GROUNDWATER_FLOW
+    solution_type = SolutionType.QUASI_STATIC
+    # Set up start and end time of calculation, time step and etc
+    time_integration = TimeIntegration(start_time=0.0, end_time=2.0, delta_time=0.01, reduction_factor=1.0,
+                                    increase_factor=1.0, max_delta_time_factor=1000)
+    convergence_criterion = DisplacementConvergenceCriteria(displacement_relative_tolerance=1.0e-4,
+                                                            displacement_absolute_tolerance=1.0e-9)
+    strategy_type = NewtonRaphsonStrategy(min_iterations=6, max_iterations=15, number_cycles=100)
+    scheme_type = NewmarkScheme(newmark_beta=0.25, newmark_gamma=0.5, newmark_theta=0.5)
+    linear_solver_settings = Amgcl(tolerance=1e-8, max_iteration=500, scaling=True)
+    stress_initialisation_type = StressInitialisationType.NONE
+    solver_settings = SolverSettings(analysis_type=analysis_type, solution_type=solution_type,
+                                    stress_initialisation_type=stress_initialisation_type,
+                                    time_integration=time_integration,
+                                    is_stiffness_matrix_constant=False, are_mass_and_damping_constant=False,
+                                    convergence_criteria=convergence_criterion,
+                                    strategy_type=strategy_type, scheme=scheme_type,
+                                    linear_solver_settings=linear_solver_settings, rayleigh_k=0.0,
+                                    rayleigh_m=0.0)
 
-# Set up solver settings
-analysis_type = AnalysisType.MECHANICAL_GROUNDWATER_FLOW
-solution_type = SolutionType.QUASI_STATIC
-# Set up start and end time of calculation, time step and etc
-time_integration = TimeIntegration(start_time=0.0, end_time=2.0, delta_time=0.01, reduction_factor=1.0,
-                                   increase_factor=1.0, max_delta_time_factor=1000)
-convergence_criterion = DisplacementConvergenceCriteria(displacement_relative_tolerance=1.0e-4,
-                                                        displacement_absolute_tolerance=1.0e-9)
-strategy_type = NewtonRaphsonStrategy(min_iterations=6, max_iterations=15, number_cycles=100)
-scheme_type = NewmarkScheme(newmark_beta=0.25, newmark_gamma=0.5, newmark_theta=0.5)
-linear_solver_settings = Amgcl(tolerance=1e-8, max_iteration=500, scaling=True)
-stress_initialisation_type = StressInitialisationType.NONE
-solver_settings = SolverSettings(analysis_type=analysis_type, solution_type=solution_type,
-                                 stress_initialisation_type=stress_initialisation_type,
-                                 time_integration=time_integration,
-                                 is_stiffness_matrix_constant=False, are_mass_and_damping_constant=False,
-                                 convergence_criteria=convergence_criterion,
-                                 strategy_type=strategy_type, scheme=scheme_type,
-                                 linear_solver_settings=linear_solver_settings, rayleigh_k=0.0,
-                                 rayleigh_m=0.0)
+    # Set up problem data
+    problem = Problem(problem_name="calculate_moving_load_on_embankment_3d", number_of_threads=1, settings=solver_settings)
+    model.project_parameters = problem
 
-# Set up problem data
-problem = Problem(problem_name="calculate_moving_load_on_embankment_3d", number_of_threads=1, settings=solver_settings)
-model.project_parameters = problem
+    # Define the results to be written to the output file
 
-# Define the results to be written to the output file
+    # Nodal results
+    nodal_results = [NodalOutput.DISPLACEMENT,
+                    NodalOutput.TOTAL_DISPLACEMENT]
+    # Gauss point results
+    gauss_point_results = [
+    ]
 
-# Nodal results
-nodal_results = [NodalOutput.DISPLACEMENT,
-                 NodalOutput.TOTAL_DISPLACEMENT]
-# Gauss point results
-gauss_point_results = [
-]
-
-# Define the output process
-vtk_output_process = Output(
-    part_name="porous_computational_model_part",
-    output_name="vtk_output",
-    output_dir="output",
-    output_parameters=VtkOutputParameters(
-        file_format="binary",
-        output_interval=1,
-        nodal_results=nodal_results,
-        gauss_point_results=gauss_point_results,
-        output_control_type="step"
+    # Define the output process
+    vtk_output_process = Output(
+        part_name="porous_computational_model_part",
+        output_name="vtk_output",
+        output_dir="output",
+        output_parameters=VtkOutputParameters(
+            file_format="binary",
+            output_interval=1,
+            nodal_results=nodal_results,
+            gauss_point_results=gauss_point_results,
+            output_control_type="step"
+        )
     )
-)
 
-# Write KRATOS input files
-# --------------------------------
+    # Write KRATOS input files
+    # --------------------------------
 
-kratos_io = KratosIO(ndim=model.ndim)
-# Define the output folder
-output_folder = "inputs_kratos"
+    kratos_io = KratosIO(ndim=model.ndim)
+    # Define the output folder
+    output_folder = "inputs_kratos"
 
-# Write project settings to ProjectParameters.json file
-kratos_io.write_project_parameters_json(
-    model=model,
-    outputs=[vtk_output_process],
-    mesh_file_name="calculate_moving_load_on_embankment_3d.mdpa",
-    materials_file_name="MaterialParameters.json",
-    output_folder=output_folder
-)
+    # Write project settings to ProjectParameters.json file
+    kratos_io.write_project_parameters_json(
+        model=model,
+        outputs=[vtk_output_process],
+        mesh_file_name="calculate_moving_load_on_embankment_3d.mdpa",
+        materials_file_name="MaterialParameters.json",
+        output_folder=output_folder
+    )
 
-# Write mesh to .mdpa file
-kratos_io.write_mesh_to_mdpa(
-    model=model,
-    mesh_file_name="calculate_moving_load_on_embankment_3d.mdpa",
-    output_folder=output_folder
-)
+    # Write mesh to .mdpa file
+    kratos_io.write_mesh_to_mdpa(
+        model=model,
+        mesh_file_name="calculate_moving_load_on_embankment_3d.mdpa",
+        output_folder=output_folder
+    )
 
-# Write materials to MaterialParameters.json file
-kratos_io.write_material_parameters_json(
-    model=model,
-    output_folder=output_folder
-)
+    model.output_settings = [vtk_output_process]
 
-# Run Kratos calculation
-# --------------------------------
+    input_folder = "benchmark_tests/test_moving_load_on_embankment_3d/inputs_kratos"
 
-project_folder = "inputs_kratos"
-os.chdir(project_folder)
+    # Write KRATOS input files
+    # --------------------------------
+    stem = Stem(model, input_folder)
+    stem.write_all_input_files()
 
-with open(project_name, "r") as parameter_file:
-    parameters = KratosMultiphysics.Parameters(parameter_file.read())
-
-model = KratosMultiphysics.Model()
-simulation = GeoMechanicsAnalysis(model, parameters)
-simulation.Run()
+    # Run Kratos calculation
+    # --------------------------------
+    stem.run_calculation()
