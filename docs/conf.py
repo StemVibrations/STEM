401641deffbb03523d8ea75bd1f3efd0f73393ff
Bruno Zuada Coelho <brunozuada@gmail.com>
2024-01-26 19:26:52 +0100

    fixed link to github in documentation

--- docs/conf.py
+++ docs/conf.py
@@ -36,8 +36,6 @@ html_theme_options = {
      'display_version': True,
      'prev_next_buttons_location': 'bottom',
      'style_external_links': True,
-     'vcs_pageview_mode': 'display_github',
-    #  'style_nav_header_background': 'white',
     # Toc options
     'collapse_navigation': False,
     'sticky_navigation': True,
