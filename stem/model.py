efe89a55e4c750690942979ee00eb13f816d0d2b
smyrniou <eleni.smyrniou@deltares.nl>
2025-02-21 11:35:51 +0100

    Corrected type

--- stem/model.py
+++ stem/model.py
@@ -78,7 +78,7 @@ class Model:
                                 sleeper_parameters: Union[NodalConcentrated, SoilMaterial],
                                 rail_pad_parameters: ElasticSpringDamper, rail_pad_thickness: float,
                                 origin_point: Sequence[float], direction_vector: Sequence[float], name: str,
-                                sleeper_dimensions: Sequence[float] = [0.0, 0.0, 0.0]):
+                                sleeper_dimensions: Optional[Sequence[float]] = None):
         """
         Generates a track geometry. With rail, rail-pads and sleepers as mass elements. Sleepers are placed at the
         bottom of the track with a distance of sleeper_distance between them. The sleepers are connected to the rail
@@ -105,6 +105,9 @@ class Model:
 
         vertical_addition_sleeper_soil_volume = 0
         if isinstance(sleeper_parameters, SoilMaterial):
+            if sleeper_dimensions is None:
+                raise ValueError("If sleeper parameters are SoilMaterial, dimensions must be a list of length, width, "
+                                 "height.")
             vertical_addition_sleeper_soil_volume = sleeper_dimensions[2]
 
         normalized_direction_vector = np.array(direction_vector) / np.linalg.norm(direction_vector)
