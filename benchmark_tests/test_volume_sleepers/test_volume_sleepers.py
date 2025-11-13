corrected rotation in sleepers

benchmark_tests/test_volume_sleepers/test_volume_sleepers.py
@@ -3,10 +3,6 @@ import sys
 from shutil import rmtree
 import numpy as np
 
-import pytest
-
-from benchmark_tests.utils import assert_files_equal
-
 from stem.model import Model
 from stem.structural_material import EulerBeam, ElasticSpringDamper
 from stem.boundary import DisplacementConstraint
@@ -30,11 +26,13 @@ from stem.soil_material import (
 )
 from stem.stem import Stem
 
+from benchmark_tests.utils import assert_floats_in_directories_almost_equal
 
-def test_moving_load_on_track():
+
+def test_point_load_on_track_static():
     ndim = 3
     model = Model(ndim)
-    model.extrusion_length = 19.5
+    model.extrusion_length = 20
 
     # Specify material model
     # Linear elastic drained soil with a Density of 2650, a Young's modulus of 30e6,
@@ -49,10 +47,10 @@ def test_moving_load_on_track():
     material1 = SoilMaterial("soil_1", soil_formulation1, constitutive_law1, retention_parameters1)
     # Specify the coordinates for the column: x:5m x y:1m
     layer1_coordinates = [
-        (0.0, 0.0, -5.0),
-        (3.47, 0.0, -5.0),
-        (3.47, 1.0, -5.0),
-        (0.0, 1.0, -5.0),
+        (0.0, 0.0, 0.0),
+        (3.47, 0.0, 0.0),
+        (3.47, 1.0, 0.0),
+        (0.0, 1.0, 0.0),
     ]
     # Create the soil layer
     model.add_soil_layer_by_coordinates(layer1_coordinates, material1, "soil_layer_1")
@@ -66,10 +64,10 @@ def test_moving_load_on_track():
     material2 = SoilMaterial("soil_2", soil_formulation2, constitutive_law2, retention_parameters2)
     # Specify the coordinates for the column: x:5m x y:1m
     layer2_coordinates = [
-        (0.0, 1.0, -5.0),
-        (3.47, 1.0, -5.0),
-        (3.47, 2.5, -5.0),
-        (0.0, 2.5, -5.0),
+        (0.0, 1.0, 0),
+        (3.47, 1.0, 0),
+        (3.47, 2.5, 0),
+        (0.0, 2.5, 0),
     ]
     # Create the soil layer
     model.add_soil_layer_by_coordinates(layer2_coordinates, material2, "soil_layer_2")
@@ -101,35 +99,33 @@ def test_moving_load_on_track():
         retention_parameters=SaturatedBelowPhreaticLevelLaw(),
     )
 
-    origin_point = np.array([2.5, 2.5, -4.5])
+    origin_point = np.array([0.75, 2.5, 0])
     direction_vector = np.array([0, 0, 1])
     # dimensions of the sleeper
     sleeper_height = 0.3
     rail_pad_thickness = 0.02
-    sleeper_length = 2.8 / 2
+    sleeper_length = 2.5 / 2
     sleeper_width = 0.234
     sleeper_distance = 1.0
-    sleeper_dimensions = [sleeper_length, sleeper_width, sleeper_height]
-    offset_sleepers_rail_pads = 0.43
+    sleeper_dimensions = [sleeper_width, sleeper_height, sleeper_length]
+    distance_middle_sleeper_to_rail = origin_point[0]
 
     # create a straight track with rails, sleepers and rail pads
-    model.generate_straight_track(
-        sleeper_distance=sleeper_distance,
-        n_sleepers=20,
-        rail_parameters=rail_parameters,
-        sleeper_parameters=sleeper_parameters,
-        rail_pad_parameters=rail_pad_parameters,
-        rail_pad_thickness=rail_pad_thickness,
-        origin_point=origin_point,
-        direction_vector=direction_vector,
-        sleeper_dimensions=sleeper_dimensions,
-        sleeper_rail_pad_offset=offset_sleepers_rail_pads,
-        name="rail_track_1",
-    )
+    model.generate_straight_track(sleeper_distance=sleeper_distance,
+                                  n_sleepers=21,
+                                  rail_parameters=rail_parameters,
+                                  sleeper_parameters=sleeper_parameters,
+                                  rail_pad_parameters=rail_pad_parameters,
+                                  rail_pad_thickness=rail_pad_thickness,
+                                  origin_point=origin_point,
+                                  direction_vector=direction_vector,
+                                  sleeper_dimensions=sleeper_dimensions,
+                                  name="rail_track_1",
+                                  distance_middle_sleeper_to_rail=distance_middle_sleeper_to_rail)
 
     load = PointLoad(active=[False, True, False], value=[0.0, -10000.0, 0.0])
     model.add_load_by_coordinates(
-        coordinates=[[2.5, 2.5 + sleeper_height + rail_pad_thickness, 5.0]],
+        coordinates=[[0.75, 2.5 + sleeper_height + rail_pad_thickness, 10.0]],
         load_parameters=load,
         name="point_load",
     )
@@ -138,18 +134,24 @@ def test_moving_load_on_track():
     no_displacement_parameters = DisplacementConstraint(active=[True, True, True],
                                                         is_fixed=[True, True, True],
                                                         value=[0, 0, 0])
-    roller_displacement_parameters = DisplacementConstraint(active=[True, True, True],
+    roller_displacement_parameters = DisplacementConstraint(active=[True, False, True],
                                                             is_fixed=[True, False, True],
                                                             value=[0, 0, 0])
 
     # Add boundary conditions to the model (geometry ids are shown in the show_geometry)
+    # model.show_geometry(show_surface_ids=True)
+
     model.add_boundary_condition_by_geometry_ids(2, [1], no_displacement_parameters, "base_fixed")
-    model.add_boundary_condition_by_geometry_ids(
-        2,
-        [2, 6, 5, 4, 444, 468, 469, 467],
-        roller_displacement_parameters,
-        "roller_fixed",
-    )
+
+    model.add_boundary_condition_on_plane([[0, 0, 0], [3.47, 0, 0], [0, 1, 0]], roller_displacement_parameters,
+                                          "roller")
+    model.add_boundary_condition_on_plane([[0, 0, 0], [0, 2.5, 0], [0, 0, model.extrusion_length]],
+                                          roller_displacement_parameters, "roller")
+    model.add_boundary_condition_on_plane([[3.47, 0, 0], [3.47, 2.5, 0], [3.47, 0, model.extrusion_length]],
+                                          roller_displacement_parameters, "roller")
+    model.add_boundary_condition_on_plane(
+        [[0, 0, model.extrusion_length], [3.47, 0, model.extrusion_length], [0, 2.5, model.extrusion_length]],
+        roller_displacement_parameters, "roller")
 
     # Set up solver settings
     analysis_type = AnalysisType.MECHANICAL
@@ -189,9 +191,6 @@ def test_moving_load_on_track():
     # Nodal results
     nodal_results = [
         NodalOutput.DISPLACEMENT,
-        NodalOutput.VELOCITY_X,
-        NodalOutput.VELOCITY_Y,
-        NodalOutput.VELOCITY_Z,
     ]
     # Gauss point results
     # gauss_point_results = [GaussPointOutput.CAUCHY_STRESS_VECTOR,GaussPointOutput.CAUCHY_STRESS_TENSOR ]
@@ -231,10 +230,9 @@ def test_moving_load_on_track():
     else:
         raise Exception("Unknown platform")
 
-    result = assert_files_equal(
+    assert_floats_in_directories_almost_equal(
         expected_output_dir,
         os.path.join(input_folder, "output/output_vtk_porous_computational_model_part"),
     )
 
-    assert result is True
     rmtree(input_folder)

stem/utils.py
@@ -5,7 +5,7 @@ import numpy as np
 import numpy.typing as npty
 from scipy.spatial import cKDTree
 
-from stem.globals import ELEMENT_DATA, GEOMETRY_PRECISION
+from stem.globals import ELEMENT_DATA
 
 if TYPE_CHECKING:
     from stem.mesh import Element, Mesh
@@ -764,54 +764,40 @@ class Utils:
             raise ValueError("Coordinates must be real numbers (not NaN or inf).")
 
     @staticmethod
-    def compute_rotational_matrix(
-        direction_vector: Sequence[float],
-    ) -> npty.NDArray[np.float64]:
+    def calculated_normal_unit_vector_to_plane(plane_points: Sequence[Sequence[float]]) -> npty.NDArray[np.float64]:
         """
-        Computes the rotation matrix to align the local z-axis with the given direction vector.
+        Calculates the normal unit vector to a plane defined by three points.
 
         Args:
-            - direction_vector (Sequence[float]): direction vector to align with
+            - plane_points (Sequence[Sequence[float]]): three points defining the plane.
+
+        Raises:
+            - ValueError: if less than three points are provided.
+            - ValueError: if the points are collinear.
 
         Returns:
-            - npty.NDArray[np.float64]: rotation matrix to align the local z-axis with the given direction vector
+            - npty.NDArray[np.float64]: normal unit vector to the plane.
 
         """
-        # Compute rotation matrix to align the local z-axis [0,0,1] with the given direction_vector
-        norm = np.array(np.linalg.norm(direction_vector), dtype=float)
-        target = direction_vector / norm
-        reference_z_axis = np.array([0.0, 0.0, 1.0])
-        # using np.clip to ensure the dot product is within the valid range
-        # as there could be floating point errors
-        dot_prod = np.clip(np.dot(reference_z_axis, target), -1.0, 1.0)
-        angle = np.arccos(dot_prod)  # The inverse of cos so that, if y = cos(x), then x = arccos(y).
 
-        # Use the identity matrix when no rotation is needed.
-        if np.abs(angle) < GEOMETRY_PRECISION:
-            R = np.eye(3)
-        else:
-            # Determine the rotation axis from the cross product.
-            axis = np.cross(reference_z_axis, target)
-            norm = np.array(np.linalg.norm(axis), dtype=float)
-            axis = axis / norm
-            cos_theta = np.cos(angle)
-            sin_theta = np.sin(angle)
-            ux, uy, uz = axis
-            row_1 = np.array([
-                cos_theta + ux**2 * (1 - cos_theta),
-                ux * uy * (1 - cos_theta) - uz * sin_theta,
-                ux * uz * (1 - cos_theta) + uy * sin_theta,
-            ])
-            row_2 = np.array([
-                uy * ux * (1 - cos_theta) + uz * sin_theta,
-                cos_theta + uy**2 * (1 - cos_theta),
-                uy * uz * (1 - cos_theta) - ux * sin_theta,
-            ])
-            row_3 = np.array([
-                uz * ux * (1 - cos_theta) - uy * sin_theta,
-                uz * uy * (1 - cos_theta) + ux * sin_theta,
-                cos_theta + uz**2 * (1 - cos_theta),
-            ])
-            # Create the rotation matrix.
-            R = np.array([row_1, row_2, row_3])
-        return R
+        if len(plane_points) < 3:
+            raise ValueError("Less than 3 points are given, cannot define a plane.")
+
+        # Convert points to a NumPy array for easier manipulation
+        p1, p2, p3 = np.array(plane_points[:3], dtype=np.float64)
+
+        # Calculate vectors from p1 to p2 and p1 to p3
+        v1 = p2 - p1
+        v2 = p3 - p1
+
+        # Calculate the normal vector of the plane formed by v1 and v2
+        normal = np.cross(v1, v2)
+
+        norm_normal = np.linalg.norm(normal)
+        if np.isclose(norm_normal, 0.0):
+            raise ValueError("The provided points are collinear, cannot define a plane.")
+
+        # Normalize the normal vector to get the unit normal vector
+        unit_normal = normal / norm_normal
+
+        return unit_normal