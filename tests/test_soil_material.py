a1cf03050a3fc8497dea3b89d0a3db03d124c636
aronnoordam <aronnoordam@gmail.com>
2025-11-17 16:04:53 +0100

    added tests for soil material

--- tests/test_soil_material.py
+++ tests/test_soil_material.py
@@ -69,3 +69,51 @@ class TestSoilMaterial:
         msg = "Property YOUNGS_MODULUS is not one of the parameters of the soil material"
         with pytest.raises(ValueError, match=msg):
             soil_material.get_property_in_material("YOUNGS_MODULUS")
+
+    def test_to_dict_serializes_soil_material(self):
+        """
+        Check that the soil material is correctly serialized to a dictionary
+        """
+
+        soil_formulation = OnePhaseSoil(ndim=3, IS_DRAINED=True, DENSITY_SOLID=2650, POROSITY=0.3)
+        constitutive_law = LinearElasticSoil(YOUNG_MODULUS=100e6, POISSON_RATIO=0.3)
+        retention_parameters = SaturatedBelowPhreaticLevelLaw()
+        fluid_properties = FluidProperties(DENSITY_FLUID=1000, DYNAMIC_VISCOSITY=1.3e-3, BULK_MODULUS_FLUID=2e9)
+        soil_material = SoilMaterial(
+            name="soil",
+            soil_formulation=soil_formulation,
+            constitutive_law=constitutive_law,
+            retention_parameters=retention_parameters,
+            fluid_properties=fluid_properties,
+        )
+        serialized = soil_material.to_dict()
+        assert serialized == {
+            "name": "soil",
+            "soil_formulation": {
+                "ndim": 3,
+                "IS_DRAINED": True,
+                "DENSITY_SOLID": 2650,
+                "POROSITY": 0.3,
+                "BULK_MODULUS_SOLID": 50e9,
+                "BIOT_COEFFICIENT": None,
+                "RAYLEIGH_M": None,
+                "RAYLEIGH_K": None,
+                "type": "OnePhaseSoil",
+            },
+            "constitutive_law": {
+                "YOUNG_MODULUS": 100e6,
+                "POISSON_RATIO": 0.3,
+                "type": "LinearElasticSoil",
+            },
+            "retention_parameters": {
+                "SATURATED_SATURATION": 1.0,
+                "RESIDUAL_SATURATION": 1e-10,
+                "type": "SaturatedBelowPhreaticLevelLaw",
+            },
+            "fluid_properties": {
+                "DENSITY_FLUID": 1000,
+                "DYNAMIC_VISCOSITY": 1.3e-3,
+                "BULK_MODULUS_FLUID": 2e9,
+                "type": "FluidProperties",
+            },
+        }
