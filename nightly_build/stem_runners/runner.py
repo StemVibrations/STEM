09ed8b942f4fc03c084cf57f9766ee73ddd1e7a2
brunozc <brunozuada@gmail.com>
2025-12-11 10:46:25 +0100

    Refactor comments in runner.py for clarity and consistency

--- nightly_build/stem_runners/runner.py
+++ nightly_build/stem_runners/runner.py
@@ -22,7 +22,7 @@ run_strip_2D(Path("./strip_2D_tmp"))
 run_strip_3D(Path("./strip_3D_tmp"))
 run_column(Path("./one_dim_wave_prop_tmp"))
 
-# compare_analytical()
+# compare_analytical
 compare_pekeris(r"pekeris_tmp/json_output.json", r"nightly_build/pekeris/time_history.pdf")
 compare_strip_load_2D(r"strip_2D_tmp/output/output_vtk_porous_computational_model_part",
                       r"nightly_build/strip_load_2D/time_history.pdf")
@@ -32,3 +32,7 @@ compare_wave_propagation(r"one_dim_wave_prop_tmp/output/calculated_output.json",
                          r"nightly_build/one_dim_wave_prop/time_history.pdf")
 
 # delete all tmp folders
+# shutil.rmtree("pekeris_tmp")
+# shutil.rmtree("strip_2D_tmp")
+# shutil.rmtree("strip_3D_tmp")
+# shutil.rmtree("one_dim_wave_prop_tmp")
